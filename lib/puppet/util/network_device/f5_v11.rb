dbded7362081b01fc8f7daf6ff5f7e5c9d5f1240
beergeek <brett.gray@puppetlabs.com>
2014-01-22 13:13:56 +1100

    Shit in wrong file

--- lib/puppet/util/network_device/f5_v11.rb
+++ lib/puppet/util/network_device/f5_v11.rb
@@ -3,22 +3,5 @@ require 'puppet/util/network_device/f5_v11/device'
 
 class Puppet::Util::NetworkDevice::F5_v11
 
-  attr_accessor :url, :transport, :partition
-
-  def initialize(url, option = {})
-    @url = URI.parse(url)
-    @option = option
-
-    unless @transport 
-      if Facter.value(:url) then
-        Puppet.debug "Puppet::Util::NetworkDevice::F5: connecting via facter url."
-        @device ||= Puppet::Util::NetworkDevice::F5_v11::Device.new(Facter.value(:url))
-      else
-        @device ||= Puppet::Util::NetworkDevice.current
-        raise Puppet::Error, "Puppet::Util::NetworkDevice::F5_v11: device not initialized #{caller.join("\n")}" unless @device
-      end
-
-      @tranport = @device.transport
-    end
 
 end
